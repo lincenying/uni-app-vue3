64170794a0e8874b4e062494f7a5b80777b810b9
林岑影 <lincenying@qq.com>
2023-04-11 01:30:35 +0800

    chore: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createSSRApp } from 'vue'
+
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 
 export function createApp() {
     const app = createSSRApp(App)
+    const store = createPinia()
+    app.use(store)
     return {
         app,
     }
